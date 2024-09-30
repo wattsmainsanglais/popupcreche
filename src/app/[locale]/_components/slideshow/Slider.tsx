f8a172cb5622924f08585fe98eb571d0b7fd58ff
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-09-30 11:12:45 +0200

    Added an image carousel component

---
+++ src/app/[locale]/_components/slideshow/Slider.tsx
@@ -0,0 +1,80 @@
+'use client'
+
+import React from "react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "popmotion";
+import { Flex } from "@radix-ui/themes";
+
+import Image from "next/image";
+
+import style from './slider.module.css'
+
+
+
+
+
+export default function Slider({images}) {
+
+const [positionIndexes, setPositionIndexes] = useState([0, 1, 2, 3, 4]);
+
+const handleNext = () => {
+    setPositionIndexes((prevIndexes) => {
+      const updatedIndexes = prevIndexes.map(
+        (prevIndex) => (prevIndex + 1) % 5
+      );
+      return updatedIndexes;
+    });
+  };
+
+    const positions = ["center", "left1", "left", "right", "right1"];
+
+    const imageVariants = {
+        center: { x: "0%", scale: 0.9, zIndex: 5 },
+        left1: { x: "-50%", scale: 0.7, zIndex: 3 },
+        left: { x: "-90%", scale: 0.5, zIndex: 2 },
+        right: { x: "90%", scale: 0.5, zIndex: 1 },
+        right1: { x: "50%", scale: 0.7, zIndex: 3 },
+    }
+    
+    return(
+        <>
+       
+        <Flex width='100vw' height='90vh' mb='5'  justify='center'>
+            <Flex justify='center' align='center'>
+
+                {images.map((image, index) => (
+                    <motion.div
+                      key={index}
+                      initial='center'
+                      animate={positions[positionIndexes[index]]}
+                      variants={imageVariants}
+                      transition={{ duration: 0.5 }}
+                      style={{ width: "40%", position: "absolute", display: 'flex', justifyContent: 'center'  }}
+
+                    >
+
+                        <Image
+                         src={image}
+                         alt='image'
+                         width={400}
+                          />
+
+                    </motion.div>
+                ))}
+
+            </Flex>
+          
+         </Flex> 
+         <button
+          className="text-white mt-[400px] bg-indigo-400 rounded-md py-2 px-4"
+          onClick={handleNext}
+        >
+          Back
+            </button> 
+         
+         </>
+    )
+
+};
+
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,16 +6,31 @@ import { Flex } from "@radix-ui/themes";
 import Splash from "./_components/splash/Splash";
 import Whatwedo from "./_components/whatwedo/WhatWeDo";
 import Meetus from "./_components/meet/MeetUs";
+import Slider from "./_components/slideshow/Slider"
+
+import s1 from '../../../public/slide1/KL2_7291web.jpg'
+import s2 from '../../../public/slide1/KL2_7633web.jpg'
+import s3 from '../../../public/slide1/KL2_7921web.jpg'
+import s4 from '../../../public/slide1/harriet-playing.jpg'
+import s5 from '../../../public/slide1/holding-banner.jpg'
+import s6 from '../../../public/slide1/sinead-playing.jpg'
 
 
 
 export default function Home() {
+
+
+
+  const images= [s1, s2, s3, s4, s5]
+
   return (
     <Flex justify='center' direction='column'>
 
       <Splash />
       <Whatwedo />
       <Meetus />
+      <Slider images={images} />
+
 
 
     </Flex>
